e5fceda7f074291f274379b371fed9eb5ff016c3
MacRae Linton <macrae.linton@gmail.com>
2018-02-07 08:21:11 -0800

    add message about closed files

--- close_orphaned_files.py
+++ close_orphaned_files.py
@@ -6,6 +6,7 @@ import os
 
 class CloseOrphanedFilesCommand(sublime_plugin.WindowCommand):
     def run(self):
+        close_count = 0
         for view in self.window.views():
             if view.file_name() is not None:
                 exists = os.path.exists(view.file_name())
@@ -14,6 +15,11 @@ class CloseOrphanedFilesCommand(sublime_plugin.WindowCommand):
                 if not exists and not view.is_dirty() and view.size() > 0:
                     view.set_scratch(True)
                     view.close()
+                    close_count += 1
+
+        if close_count > 0:
+            plural = "s" if close_count > 1 else ""
+            self.window.status_message("Closed {} orphaned file{}.".format(close_count, plural))
 
 
 class CloseOrphandFilesEventListener(sublime_plugin.EventListener):
