109aa710ed3051ca82e8e3d90bed331c61f8b335
MacRae Linton <macrae.linton@gmail.com>
2018-02-06 23:40:46 -0800

    fix up some comments, pretty much ready to go

--- close_orphaned_files.py
+++ close_orphaned_files.py
@@ -10,16 +10,14 @@ class CloseOrphanedFilesCommand(sublime_plugin.WindowCommand):
             if view.file_name() is not None:
                 exists = os.path.exists(view.file_name())
 
-                # is_dirty is not set for a file that has been deleted
-                # If there have been any edits since the deletion, then we won't close the buffer
+                # is_dirty is not set for a file that has just been deleted
                 if not exists and not view.is_dirty() and view.size() > 0:
                     view.set_scratch(True)
                     view.close()
 
-# Just do it on focus for the window. That's something you do after you go do a bunch of git commands. you switch back and then it should prune
-# There's edges here for sure. but not big ones.
 
 class CloseOrphandFilesEventListener(sublime_plugin.EventListener):
+    # Just do it on focus for the window. That's something you do after you go
+    # do a bunch of git commands. you switch back and then it should prune
     def on_activated(self, view):
         view.window().run_command('close_orphaned_files')
-
